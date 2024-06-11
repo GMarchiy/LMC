1a7dbb561ef608cfc9e3dc16fee68e1b125a2a43
GMarchiy <georgiy.marchiy@mail.ioffe.ru>
2024-06-11 15:17:38 +0300

    self-adjusting of mu, kappa, temp

--- vcsgc_numba.py
+++ vcsgc_numba.py
@@ -96,8 +96,24 @@ Xtot = 10/100
 Nsites = Fsize*M
 
 T = 2000
+Tf = 100
 mu = 79
-kappa = 100
+n_mu = 10 #adjust mu every plot_each*n_mu
+dmu = 1 #step for adjusting mu (mu = mu + (c-c0)*dmu/100)
+
+kappa = 10000
+n_kappa = 100 #adjust kappa every plot_each*n_kappa
+dkappa = np.sqrt(10) # kappa = kappa *dkappa
+ac_treshold = 0.05 # decrease kappa if acc_ratio < ac_treshold
+cv_treshold = 5 # increase kappa if std > cv_treshold
+
+Neq = int(2e6)
+Ncool = int(1e6)
+Nstd = int(2e6)
+T_each = Neq+Ncool+Nstd
+k_f = 2e-1/Ncool
+k_s = 1e-1/Ncool
+estd_c = 0.4
 
 Nsteps = 1e8
 print_each = 1000
@@ -137,10 +153,12 @@ E = 0
 Etot = 0
 if rank == 0:
     es = np.zeros(int(Nsteps//save_each+1), dtype=np.float64)
+    ts = np.zeros(int(Nsteps//save_each+1), dtype=np.float64)
     cs = np.zeros(int(Nsteps//save_each+1), dtype=np.float64)
     acs = np.zeros(int(Nsteps//save_each+1), dtype=np.float64)
     
-while steps<Nsteps:
+cooling = False
+while T>Tf:
     accepted, Ntot, dE = step(s, T, mu, kappa, c0, 
     Eseg, w, Fsize, Ntot, rng)
     accepteds += accepted
@@ -162,6 +180,7 @@ while steps<Nsteps:
             es[n] = Etot
             cs[n] = 100*Ntot/Nsites
             acs[n] = accepteds_tot/save_each/Ncpu
+            ts[n] = T
             
     if steps%plot_each==0:
         xs = np.sum((s+1)/2, axis=1).astype(np.float32)
@@ -169,10 +188,26 @@ while steps<Nsteps:
         MPI.COMM_WORLD.Reduce([xs, M], [xstot, M],
         op=MPI.SUM, root=0)
 
-        if rank == 0:
+        if rank == 0 and steps>0:
+            n = steps//save_each
+            n0 = (steps-plot_each*n_mu)//save_each
+            n1 = (steps-plot_each*n_kappa)//save_each
+            cm = cs[n0:n+1].mean()
+            cv = cs[n1:n+1].std()
+            acm = acs[n1:n+1].mean()
+            if steps%int(plot_each*n_mu)==0:
+                mu = mu+(cm/100-c0)*dmu
+            if steps%int(plot_each*n_kappa)==0:
+                if cv>cv_treshold:
+                    kappa *= dkappa
+                elif acm<ac_treshold:
+                    kappa /= dkappa
+
             scale = 2
             plt.figure(figsize=(15/scale, 5/scale))
             plt.subplot(131)
+            plt.plot(ts[:n+1], color='red')
+            plt.twinx()
             plt.plot(es[:n+1])
             plt.subplot(132)
             xstot = xstot/Fsize
@@ -181,17 +216,46 @@ while steps<Nsteps:
             plt.plot(acs[:n+1], color='red')
             plt.twinx()
             plt.plot(cs[:n+1])
-            plt.text(n*0.75, cs.max()*0.7, round(cs[:n+1].std(), 3))
+            plt.text(n*0.5, cs.max()*0.5, f'std {round(cv, 3)}')
+            plt.text(n*0.2, cs.max()*0.1, f'kappa {round(kappa, 0)}')
+            plt.text(n*0.5, cs.max()*0.3, f'mu {round(mu, 2)}')
             plt.gcf().tight_layout()
             plt.savefig(f'plots/step.png')
             plt.close()
+        if steps%int(plot_each*n_mu)==0:
+            mu = MPI.COMM_WORLD.bcast(mu, root=0)
+        if steps%int(plot_each*n_kappa)==0:
+            kappa = MPI.COMM_WORLD.bcast(kappa, root=0)
 
-        if steps%dump_each==0:
-            stot = np.empty((M,Fsize), dtype=np.int8)
-            MPI.COMM_WORLD.Gather(s, stot, root=0)
-            if rank==0:
-                with open('s.dump', 'wb') as f:
-                    pickle.dump(stot, f)
+    if steps%T_each==0 and steps>0:
+        cooling = True
+        cool_step = 0
+        if rank == 0:
+            n0 = (steps-Nstd)//save_each
+            n = steps//save_each
+            estd = es[n0:n+1].std()
+            with open('estd.txt', 'a') as f:
+                f.write(f'{steps} {T} {estd}\n')
+        else:
+            estd = None
+        estd = MPI.COMM_WORLD.bcast(estd, root=0)
+        if estd>= estd_c:
+            k = k_s
+        else:
+            k = k_f
+    if cooling:
+        if cool_step < Ncool:
+            T *= (1-k)
+            cool_step += 1
+        else:
+            cooling = False
+
+    if steps%dump_each==0:
+        stot = np.empty((M,Fsize), dtype=np.int8)
+        MPI.COMM_WORLD.Gather(s, stot, root=0)
+        if rank==0:
+            with open('s.dump', 'wb') as f:
+                pickle.dump(stot, f)
         
         
         
